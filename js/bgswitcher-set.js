839114dcc7f7ea6d20f5f8cdb34e7be25f10ec58
Ruka Sugai <rukasugai114@gmail.com>
2020-09-27 11:24:13 +0900

    初コミット

---
+++ js/bgswitcher-set.js
@@ -0,0 +1,6 @@
+jQuery(function($) {
+    $('header').bgSwitcher({
+      images: ["images/hero.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg", "images/background5.jpg"],
+      interval: 7000,
+    });
+});
